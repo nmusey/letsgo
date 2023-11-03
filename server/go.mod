c0273fa7c86d7f49d8ceae881380f9d24c7e1332
Nick Musey <nmusey@gmail.com>
2023-11-03 14:49:35 -0700

    Remove old router, refactor dirs

--- server/go.mod
+++
@@ -1,6 +0,0 @@
-module github.com/nmusey/letsgo/server
-
-go 1.21
-
-require (
-)
--- server/http/middleware/validate.go
+++
@@ -1,9 +0,0 @@
-package middleware
-
-import "github.com/nmusey/letsgo/server/http"
-
-func MakeValidator(validator func (interface{}) error) func (ctx http.Ctx) error {
-    return func (ctx http.Ctx) error {
-        return validator(ctx)
-    }
-}
