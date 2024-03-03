a9a0f995da10c2ce0678f1e31dc45c43b894e3a4
Nick Musey <nmusey@gmail.com>
2024-03-03 10:33:09 -0800

    Fix templ compile issues

--- _templates/pkg/core/http-utils.go
+++ _templates/pkg/core/http-utils.go
@@ -1,10 +1,13 @@
 package core
 
 import (
-    "net/http"
-    "encoding/json"
+	"context"
+	"encoding/json"
+	"net/http"
 
-    _ "github.com/a-h/templ"
+	"github.com/a-h/templ"
+
+	"$appRepo/views/layouts"
 )
 
 func WriteJSON(w http.ResponseWriter, payload interface{}) error {
@@ -16,7 +19,6 @@ func ReadJSON(r *http.Request, payload interface{}) error {
     return json.NewDecoder(r.Body).Decode(payload)
 }
 
-func RenderTemplate(w http.ResponseWriter, name string, data interface{}) error {
-    // TODO: Implement template rendering with templ
-    return nil
+func RenderTemplate(w http.ResponseWriter, components templ.Component) {
+    layouts.MainLayout(components).Render(context.Background(), w)
 }
