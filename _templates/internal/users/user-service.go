e147872f3f104544e1b4de7e950d203281b3ab3b
Nick Musey <nmusey@gmail.com>
2024-09-18 23:22:49 -0700

    Run formatter

--- _templates/internal/users/user-service.go
+++ _templates/internal/users/user-service.go
@@ -5,42 +5,41 @@ import (
 )
 
 type SQLUserService struct {
-    router core.Router
+	router core.Router
 }
 
 func NewUserService(router core.Router) SQLUserService {
-    return SQLUserService{
-        router: router,
-    }
+	return SQLUserService{
+		router: router,
+	}
 }
 
 func (u SQLUserService) SaveUser(user *User) error {
-    query := "INSERT INTO users(email) VALUES (:email)"
-    _, err := u.router.DB.NamedExec(query, user)
-    return err
+	query := "INSERT INTO users(email) VALUES (:email)"
+	_, err := u.router.DB.NamedExec(query, user)
+	return err
 }
 
 func (u SQLUserService) GetUsers() ([]User, error) {
-    var users []User
-    query := "select * from users";
+	var users []User
+	query := "select * from users"
 
-    err := u.router.DB.Select(&users, query)
-    return users, err
+	err := u.router.DB.Select(&users, query)
+	return users, err
 }
 
 func (u SQLUserService) GetUserById(id int) (*User, error) {
-    user := &User{}
-    query := "SELECT * FROM users WHERE id = $1"
+	user := &User{}
+	query := "SELECT * FROM users WHERE id = $1"
 
-    err := u.router.DB.Get(user, query, id)
-    return user, err
+	err := u.router.DB.Get(user, query, id)
+	return user, err
 }
 
 func (u SQLUserService) GetUserByEmail(email string) (*User, error) {
-    user := &User{}
-    query := "SELECT * FROM users WHERE email = $1"
+	user := &User{}
+	query := "SELECT * FROM users WHERE email = $1"
 
-    err := u.router.DB.Get(user, query, email)
-    return user, err
+	err := u.router.DB.Get(user, query, email)
+	return user, err
 }
-
