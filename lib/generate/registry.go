2c315401908349a19c6d584d2a13cd56e8aa112a
Nick Musey <nmusey@gmail.com>
2026-09-17 18:06:27 -0700

    Refactor generated package architecture

--- lib/generate/registry.go
+++ lib/generate/registry.go
@@ -1,17 +1,18 @@
 package generate
 
 type Kind struct {
-	Name string
+	Name         string
 	TemplateFile string
-	OutputFile string
-	FlagLong  string
-	FlagShort string
+	OutputPath string
+	FlagLong   string
+	FlagShort  string
 }
 
 var Kinds = []Kind{
-	{Name: "model", TemplateFile: "model.go.template", OutputFile: "model.go", FlagLong: "model", FlagShort: "m"},
-	{Name: "repository", TemplateFile: "repository.go.template", OutputFile: "repository.go", FlagLong: "repository", FlagShort: "r"},
-	{Name: "service", TemplateFile: "service.go.template", OutputFile: "service.go", FlagLong: "service", FlagShort: "s"},
+	{Name: "model", TemplateFile: "model.go.template", OutputPath: "lib/domain/{{.Package}}/model.go", FlagLong: "model", FlagShort: "m"},
+	{Name: "service", TemplateFile: "service.go.template", OutputPath: "lib/domain/{{.Package}}/service.go", FlagLong: "service", FlagShort: "s"},
+	{Name: "app-service", TemplateFile: "appservice.go.template", OutputPath: "lib/application/{{.Package}}/{{.Package}}service.go", FlagLong: "app-service", FlagShort: "a"},
+	{Name: "repository", TemplateFile: "repository.go.template", OutputPath: "lib/infrastructure/database/{{.Package}}/repository.go", FlagLong: "repository", FlagShort: "r"},
 }
 
 func KindByName(name string) (Kind, bool) {
